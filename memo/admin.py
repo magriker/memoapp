0795a9120dbd5c6808d2084d33fa468417fa5528
magriker <magken7@gmail.com>
2025-01-08 00:10:07 +0900

    commit

--- memo/admin.py
+++ memo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Memo
 
 # Register your models here.
+
+
+class MemoAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content")
+    search_filelds = "title"
+
+
+admin.site.register(Memo, MemoAdmin)
