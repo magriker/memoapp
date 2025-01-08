aa77a5b4841f3ad83a3a3d6d70a6771bfe5caa55
magriker <magken7@gmail.com>
2025-01-09 01:11:39 +0900

    commit

--- memo/views.py
+++ memo/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from .models import Memo
 
 # Create your views here.
 
 
 class TopView(TemplateView):
     template_name = "top.html"
+
+
+class MemoCreateView(CreateView):
+    model = Memo
+    fields = "__all__"
